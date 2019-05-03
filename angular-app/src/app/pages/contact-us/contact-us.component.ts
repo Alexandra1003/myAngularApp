3c8285c364e5e580026484820f1a01e4a39dd50a
Alexandra1003 <alexandrapanasenko10.03@gmail.com>
2019-05-03 13:16:43 +0300

    feature/contact-us *add built-in validation for contact-us form

--- angular-app/src/app/pages/contact-us/contact-us.component.ts
+++ angular-app/src/app/pages/contact-us/contact-us.component.ts
@@ -8,9 +8,38 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class ContactUsComponent implements OnInit {
 
+  myForm: FormGroup;
+  name: FormControl;
+  comment: FormControl;
+
   constructor() { }
 
+  createFormControls() {
+    this.name = new FormControl('', Validators.required);
+    this.comment = new FormControl('', Validators.required);
+  }
+
+  createForm() {
+    this.myForm = new FormGroup({
+      name: this.name,
+      comment: this.comment
+    });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    if (this.myForm.valid) {
+      console.log('it is valid');
+      this.myForm.reset();
+    } else {
+      console.log('it is invalid');
+    }
+  }
+
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
   }
 
 }
